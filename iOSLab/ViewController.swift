2b059f8d7795bd8e3633acf32f247c381b864666
Andrey Minin <andreyminin@mbp-andrey>
2019-01-09 20:26:23 +0300

    Initial Commit

---
+++ iOSLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSLab
+//
+//  Created by Andrey Minin on 09/01/2019.
+//  Copyright © 2019 Andrey Minin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
